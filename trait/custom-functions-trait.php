265c29a4a7913a397d3cff83b5f0fc88c73c9c4e
stepp <stepp@mail.com>
2023-10-22 18:25:59 +0200

    Updated enqueue_bootstrap_scripts

--- trait/custom-functions-trait.php
+++ trait/custom-functions-trait.php
@@ -2,6 +2,8 @@
 
 namespace Custom404Page\Trait;
 
+use Custom404Page\Interfaces\Constants as C;
+
 /**
  * Trait used by CustomFunctions and AdminCustomFunctions classes
  */
@@ -11,19 +13,23 @@ trait CustomFunctionsTrait{
      * Insert the bootstrap CSS and JS main files
      */
     public function enqueue_bootstrap_scripts(){
-        $styles = wp_styles();
-        $pattern = '/Bootstrap/i';
-        array_walk($styles->queue, function($key,$value) use($pattern){
-            if(preg_match($pattern, $value)) wp_dequeue_style($value);
-        });
-        $scripts = wp_scripts();
-        array_walk($scripts->queue, function($key,$value) use($pattern){
-            if(preg_match($pattern, $value)) wp_dequeue_script($value);
-        });
-        $bootstrapCss = "";
-        $bootstrapJs = "";
-        wp_enqueue_style('BootstrapCss',$bootstrapCss,[],'5.3.2');
-        wp_enqueue_script('BootstrapJs',$bootstrapJs,[],'5.3.2',true);
+        $bootstrapCssAbs = WP_PLUGIN_DIR.C::BOOSTRAP_CSS_RELATIVE;
+        $bootstrapJsAbs = WP_PLUGIN_DIR.C::BOOSTRAP_JS_RELATIVE;
+        if(file_exists($bootstrapCssAbs) && file_exists($bootstrapJsAbs)){
+            $styles = wp_styles();
+            $pattern = '/Bootstrap/i';
+            array_walk($styles->queue, function($key,$value) use($pattern){
+                if(preg_match($pattern, $value)) wp_dequeue_style($value);
+            });
+            $scripts = wp_scripts();
+            array_walk($scripts->queue, function($key,$value) use($pattern){
+                if(preg_match($pattern, $value)) wp_dequeue_script($value);
+            });
+            $bootstrapCss = plugins_url().C::BOOSTRAP_CSS_RELATIVE;
+            $bootstrapJs = plugins_url().C::BOOSTRAP_JS_RELATIVE;
+            wp_enqueue_style('BootstrapCss',$bootstrapCss,[],'5.3.2');
+            wp_enqueue_script('BootstrapJs',$bootstrapJs,[],'5.3.2',true);
+        }
     }
 
 
