2f627ec7c86519c11bb9315bde8970d3e14c0ad2
stepp <stepp@mail.com>
2023-10-22 19:48:02 +0200

    Updated file names

--- custom_404_page.php
+++ custom_404_page.php
@@ -11,23 +11,17 @@
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: custom-404-page
  */
-require_once './interfaces/constants.php';
-require_once './trait/custom-functions-trait.php';
-require_once './classes/activation-deactivation.php';
-require_once './classes/admin-custom-functions.php';
-require_once './classes/custom-functions.php';
 
 use Custom404Page\Classes\ActivationDeactivation;
 use Custom404Page\Classes\AdminCustomFunction;
-use Custom404Page\Classes\CustomFunctions;
+use Custom404Page\Classes\WpCustomFunctions;
 
 $ad = new ActivationDeactivation($GLOBALS['wpdb']);
+$cf = new WpCustomFunctions();
+$acf = new AdminCustomFunction();
 
 register_activation_hook(__FILE__,[$ad,'activate']);
 register_uninstall_hook(__FILE__,[$ad,'uninstall']);
-
-$cf = new CustomFunctions();
-$acf = new AdminCustomFunction();
 
 add_action('admin_enqueue_scripts',[$acf,'enqueue_bootstrap_scripts'],11);
 add_action('admin_menu',[$acf,'custom_404_page_menu']);
