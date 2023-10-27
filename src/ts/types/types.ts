9e4041485b1bc06616adbd4a63f5a30e289c26d0
stepp <stepp@mail.com>
2023-10-27 22:09:18 +0200

    Added types.ts

---
+++ src/ts/types/types.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Type for AdminCustomPageMenuEvents class
+ */
+export type AdminCustomPageMenuEventsType = {
+  
+    cb_enable_page: HTMLInputElement;
+    cb_enable_image: HTMLInputElement;
+    input_file_image: HTMLInputElement;
+    cb_enable_text: HTMLInputElement;
+    ta_text: HTMLTextAreaElement;
+    cb_show_articles: HTMLInputElement;   
+}
