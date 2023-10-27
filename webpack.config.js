77a881df1e323cdb0f1b2c1c2da9c4d2c36e4730
stepp <stepp@mail.com>
2023-10-27 21:59:19 +0200

    Added src/ts folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,8 @@ const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 
 module.exports = {
     entry: {
-        'css/wp/nl_wp': join(scssPath,'wp/nl_wp.scss'),
-        'css/wp/verify': join(scssPath,'wp/verify.scss'),
-        'css/admin/nl_admin_add': join(scssPath,'admin/nl_admin_add.scss'),
-        'css/admin/nl_admin_delete': join(scssPath,'admin/nl_admin_delete.scss'),
-        'css/admin/nl_admin_log': join(scssPath,'admin/nl_admin_log.scss'),
-        'css/admin/nl_admin_send': join(scssPath,'admin/nl_admin_send.scss'),
-        'css/admin/nl_admin_settings': join(scssPath,'admin/nl_admin_settings.scss'),
-        'js/wp/nl_wp': join(tsPath,'wp/nl_wp.ts'),
-        'js/admin/nl_admin_add': join(tsPath,'admin/nl_admin_add.ts'),
-        'js/admin/nl_admin_delete': join(tsPath,'admin/nl_admin_delete.ts'),
-        'js/admin/nl_admin_log': join(tsPath, 'admin/nl_admin_log.ts'),
-        'js/admin/nl_admin_send': join(tsPath,'admin/nl_admin_send.ts'),
-        'js/admin/nl_admin_settings': join(tsPath,'admin/nl_admin_settings.ts'),
-        'js/scripts/preunsubscribe': join(tsPath,'scripts/preunsubscribe.ts'),
-        'js/create_users': join(tsPath,'create_users.ts')
+        'css/admin_custom_page_menu': join(scssPath,'admin_custom_page_menu.scss'),
+        'js/admin_custom_page_menu': join(scssPath,'admin_custom_page_menu.ts')
     },
     output: {
         path: resolve(__dirname,'dist'),
